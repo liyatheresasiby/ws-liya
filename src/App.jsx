990db1f46c56f0e8dda31e7c386e1b5422b3e514
liyatheresasiby <102028659+liyatheresasiby@users.noreply.github.com>
2024-03-16 15:18:39 +0530

    updated appname

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,16 @@ import { HeroBullets } from "./components/HeroBullets/HeroBullets";
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
-const App = () => (
-  <MantineProvider theme={theme}>
-    <div className="container">
-      <div>
-        <HeroBullets />
-      </div>
-    </div>
-  </MantineProvider>
-);
-ReactDOM.render(<App />, document.getElementById("app"));
+export default () => {
+  return (
+    <>
+      <MantineProvider theme={theme}>
+        <div className="container">
+          <div>
+            <HeroBullets />
+          </div>
+        </div>
+      </MantineProvider>
+    </>
+  );
+};
