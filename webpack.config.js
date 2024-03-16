ca84dbe816c7d88dc1442b6507b3a3aa45268922
liyatheresasiby <102028659+liyatheresasiby@users.noreply.github.com>
2024-03-16 13:18:41 +0530

    build commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3010/",
+    publicPath: "auto",
   },
 
   resolve: {
@@ -60,6 +60,6 @@ module.exports = (_, argv) => ({
     new HtmlWebPackPlugin({
       template: "./src/index.html",
     }),
-    new Dotenv()
+    new Dotenv(),
   ],
 });
